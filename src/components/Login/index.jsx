f8840fc3ffcc9b3a1b465bb2eb1fabaf27ca0dda
Renan Nascimento <atomsfrontend@gmail.com>
2025-02-16 20:09:29 -0300

    feat: created fetch to do login request

--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -1,6 +1,10 @@
 // Syled Components
 import * as S from "./styles";
 
+// Hooks
+import { useNavigate } from "react-router-dom";
+import Swal from "sweetalert2";
+
 // Types
 import P from "prop-types";
 
@@ -8,6 +12,53 @@ import P from "prop-types";
 import Head from "../../components/helpers/Head";
 
 export const Login = ({ csrfToken }) => {
+    const navigate = useNavigate();
+
+    const fetchLogin = async (e) => {
+        e.preventDefault();
+
+        const form = e.target;
+        const data = new FormData(form)
+
+        const email = data.get("email");
+        const password = data.get("password");
+
+        try {
+            const response = await fetch('http://localhost:3000/login', {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    "X-CSRF-Token": csrfToken,  // Enviando o token no cabeçalho
+                },
+                credentials: 'include',
+                body: JSON.stringify({ email, password }),
+            })
+            const data = await response.json();
+
+            if (!data.errors) {
+                Swal.fire({
+                    title: "Sucesso!",
+                    text: `${data.success[0]}`,
+                    icon: "success",
+                    confirmButtonText: "OK",
+                    confirmButtonColor: "#111111d9",
+                });
+                navigate("/");
+            }
+
+            data.errors && Swal.fire({
+                title: "Error!",
+                text: `${data.errors[0]}`,
+                icon: "error",
+                confirmButtonText: "OK",
+                confirmButtonColor: "#111111d9",
+            });
+
+        } catch (error) {
+            console.log('error ao enviar dados pela rota /login.', error);
+        }
+    }
+
     return (
         <S.Container>
             <Head
@@ -17,7 +68,7 @@ export const Login = ({ csrfToken }) => {
 
             <S.Form>
                 <h2> Faça Login no Sistema </h2>
-                <form action="http://localhost:3000/login" method="post">
+                <form onSubmit={(e) => fetchLogin(e)}>
                     <div className="form-group">
                         <label htmlFor="email">
                             Email
