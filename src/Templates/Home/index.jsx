16983425c4efe39d795ffdf64cf59654eafd7682
Renan Nascimento <atomsfrontend@gmail.com>
2025-02-15 21:14:36 -0300

    feat: created component Home

--- src/Templates/Home/index.jsx
+++ src/Templates/Home/index.jsx
@@ -1,9 +1,8 @@
-import { useEffect } from "react";
+import React from "react";
+import { Link } from "react-router-dom";
 
 // Syled Components
 import * as S from "./styles";
-
-// Components
 
 // Helpers
 import Head from "../../components/helpers/Head";
@@ -12,14 +11,30 @@ import Head from "../../components/helpers/Head";
 import AOS from "aos";
 import "aos/dist/aos.css";
 
-const Home = () => {
+// Types
+import P from "prop-types";
 
-  useEffect(() => {
+const Home = ({ contatos }) => {
+
+  React.useEffect(() => {
     AOS.init({
-      duration: 1500,
+      duration: 2500,
       once: true,
     });
   }, []);
+
+  const formatDate = (date) => {
+    const newDate = new Date(date);
+
+    return new Intl.DateTimeFormat('pt-BR', {
+      day: '2-digit',
+      month: '2-digit',
+      year: 'numeric',
+      hour: '2-digit',
+      minute: '2-digit',
+      hour12: false
+    }).format(newDate)
+  }
 
   return (
     <S.Content>
@@ -28,8 +43,53 @@ const Home = () => {
         description="Agenda SyS, é um sistema de cadatros de contatos para serem visualizados como uma Agenda."
       />
 
-      <p>Teste</p>
+      <S.Contacts>
+        <h1>Agenda</h1>
+        <p >Seus contatos estão abaixo</p>
+        <span></span>
+
+        {console.log("Home: ", contatos)}
+        {contatos ? (
+          <S.Table>
+            {contatos.map(contato => (
+              <tr key={contato.id}>
+                <td>{contato.name}</td>
+                <td>{contato.tel}</td>
+                <td>{contato.email}</td>
+                <td>{formatDate(contato.created)}</td>
+                <td>
+                  <button id="edit">
+                    <Link to={`/contato/index/${contato.id}`}>
+                      Editar
+                    </Link>
+                  </button>
+                </td>
+                <td>
+                  <button id="delete">
+                    <Link to={`/contato/delete/${contato.id}`}>
+                      Excluir
+                    </Link>
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </S.Table>
+        ) : (
+          <p>Não existem contatos na sua agenda.</p>
+        )}
+      </S.Contacts>
     </S.Content>
   );
 };
+Home.PropTypes = {
+  contatos: P.arrayOf(
+    P.shape({
+      name: P.string.isRequired,
+      tel: P.string.isRequired,
+      email: P.string.isRequired,
+      created: P.string.isRequired,
+    })
+  ).isRequired
+}
+
 export default Home;
--- src/Templates/Home/styles.js
+++ src/Templates/Home/styles.js
@@ -1,3 +1,66 @@
 import styled from "styled-components";
 
-export const Content = styled.div``;
+export const Content = styled.main`
+    height: calc(100vh - 60px);
+
+    display: flex;
+    justify-content: center;
+`;
+
+export const Contacts = styled.section`
+    width: 700px;
+    height: 100%;
+
+    gap: .575rem;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+
+    padding: .575rem;
+
+    h1 {
+        font: var(--poppins-32);
+    }
+
+    p {
+        font: var(--poppins-16);
+    }
+
+    span {
+        width: 80%;
+
+        border: 1px solid #ebe8e8;
+    }
+`;
+
+export const Table = styled.table`
+    width: 100%;
+
+    tr > td > button {
+        border: none;
+        border-radius: 4px;
+
+        padding: .425rem;
+    }
+
+    tr > td > #edit {
+        background: var(--color-p5);
+
+        &:hover {
+            background: var(--color-p6);
+        }
+    }
+
+    tr > td > #delete {
+        background: var(--color-13);
+
+        &:hover {
+            background: var(--color-12);
+        }
+    }
+
+    tr > td > button > a {
+        font: var(--poppins-12);
+        color: var(--color-0)
+    }
+`;
