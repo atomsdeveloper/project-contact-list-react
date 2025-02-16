ef378050d2bae277db5b7537f6ce271de7484483
Renan Nascimento <atomsfrontend@gmail.com>
2025-02-15 21:16:36 -0300

    feat: added contacts and csrfToken fetch api

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
-import { Routes, Route, BrowserRouter } from "react-router-dom";
-import { Suspense, lazy } from "react";
 
+import React, { Suspense, lazy } from "react";
+
+// Navegation
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 
 // Components
-import { NotFound } from "./components/NotFound"
+import { NotFound } from "./components/NotFound";
+import { Header } from "./components/Header";
+import { Login } from "./components/Login";
 
 // Lazy Components
 const Home = lazy(() => import("./Templates/Home"));
@@ -16,13 +20,39 @@ export const Loading = () => {
 }
 
 function App() {
+  const [contatos, setContatos] = React.useState([]);
+  const [csrfToken, setCsrfToken] = React.useState("");
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch("http://localhost:3000", {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        credentials: "include",
+      });
+      const datas = await response.json();
+
+
+      setContatos(datas.contatos);
+      setCsrfToken(datas.csrfToken);
+    } catch (error) {
+      console.log("Error ao buscar dados.", error);
+    }
+  }
+  React.useEffect(() => {
+    fetchData()
+  }, [])
+
   return (
     <BrowserRouter>
-      <Suspense fallback={<Loading />}>
-        <div>Header</div>
+      <Header />
 
+      <Suspense fallback={<Loading />}>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Home contatos={contatos} />} />
+          <Route path="/login" element={<Login csrfToken={csrfToken} />} />
           <Route path="*" element={<NotFound text="Página não encontrada." />} />
         </Routes>
       </Suspense>
