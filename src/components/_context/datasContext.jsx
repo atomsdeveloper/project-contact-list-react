dba3c7f83bec32a06ceb3a079159bc0daee9dc65
Renan Nascimento <atomsfrontend@gmail.com>
2025-02-19 22:37:33 -0300

    feat: created context to contact all data

---
+++ src/components/_context/datasContext.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import P from "prop-types";
+
+// eslint-disable-next-line react-refresh/only-export-components
+export const ContactContext = React.createContext();
+
+export const ContactProvider = ({ children }) => {
+  const [contatos, setContatos] = React.useState([]);
+  const [csrfToken, setCsrfToken] = React.useState("");
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch("http://localhost:3000", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        credentials: "include",
+      });
+      const datas = await response.json();
+
+      setContatos(datas.contatos);
+      setCsrfToken(datas.csrfToken);
+    } catch (error) {
+      console.log("Erro ao buscar dados de contatos e csrfToken.", error);
+    }
+  };
+
+  React.useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <ContactContext.Provider value={{ contatos, csrfToken, fetchData }}>
+      {children}
+    </ContactContext.Provider>
+  );
+};
+ContactProvider.propTypes = {
+  children: P.node.isRequired,
+};
