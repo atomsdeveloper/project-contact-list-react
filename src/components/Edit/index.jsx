f466a275a38f105c839c76a0e330b8a463d4821d
Renan Nascimento <atomsfrontend@gmail.com>
2025-02-19 22:36:34 -0300

    feat: created edit contact component

--- src/components/Edit/index.jsx
+++ src/components/Edit/index.jsx
@@ -1,3 +1,171 @@
+import React from "react";
+
+import * as S from "./styles";
+
+import Head from "../_helpers/Head";
+
+// Context
+import { ContactContext } from "../_context/datasContext"; // Importa o contexto
+
+// Hooks
+import { useParams, useNavigate } from "react-router-dom";
+
+// Types
+import P from "prop-types";
+
+// Lib
+import Swal from "sweetalert2";
+
 export const Edit = () => {
-  return <h1>Editar contato</h1>;
+  const navigate = useNavigate();
+  const { id } = useParams();
+  const { fetchData, csrfToken } = React.useContext(ContactContext); // Usa o contexto
+  const [formData, setFormData] = React.useState({
+    name: "",
+    secondname: "",
+    email: "",
+    tel: "",
+  });
+
+  const fetchBuscarId = async () => {
+    try {
+      const response = await fetch(
+        `http://localhost:3000/contato/index/${id}`,
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            "X-CSRF-Token": csrfToken,
+          },
+          credentials: "include",
+        }
+      );
+      const data = await response.json();
+
+      if (data && data.contato) {
+        setFormData({
+          name: data.contato.name || "",
+          secondname: data.contato.secondname || "",
+          email: data.contato.email || "",
+          tel: data.contato.tel || "",
+        });
+      }
+
+      if (!data.success) {
+        Swal.fire({
+          title: "Error!",
+          text: `${data.message}`,
+          icon: "error",
+          confirmButtonText: "OK",
+          confirmButtonColor: "#111111d9",
+        }).then(() => {
+          navigate("/");
+        });
+      }
+    } catch (error) {
+      console.log("Error ao buscar dados de contatos para editar.", error);
+    }
+  };
+
+  React.useEffect(() => {
+    fetchBuscarId();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  // Atualiza o estado quando o usuário digita no formulário
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault(); // Evita recarregar a página
+
+    try {
+      const response = await fetch(`http://localhost:3000/contato/edit/${id}`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "X-CSRF-Token": csrfToken,
+        },
+        credentials: "include",
+        body: JSON.stringify(formData),
+      });
+
+      const data = await response.json();
+
+      if (!data.errors) {
+        Swal.fire({
+          title: "Sucesso!",
+          text: `${data.message}`,
+          icon: "success",
+          confirmButtonText: "OK",
+          confirmButtonColor: "#111111d9",
+        }).then(() => {
+          fetchData(); // Atualiza os contatos sem recarregar
+          navigate("/");
+        });
+      }
+    } catch (error) {
+      console.error("Erro ao atualizar contato:", error);
+    }
+  };
+
+  return (
+    <S.Container>
+      <Head
+        title="Cadastro Contato"
+        description="Agenda SyS, é um sistema de cadatros de contatos para serem visualizados como uma Agenda."
+      />
+      <S.Form>
+        <h2> Edit um contato no sistema. </h2>
+        <form onSubmit={handleSubmit}>
+          <div className="form-group">
+            <label htmlFor="name">Nome</label>
+            <input
+              type="name"
+              name="name"
+              value={formData.name}
+              onChange={(e) => handleChange(e)}
+            />
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="secondname">Sobrenome</label>
+            <input
+              type="secondname"
+              name="secondname"
+              value={formData.secondname}
+              onChange={(e) => handleChange(e)}
+            />
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              name="email"
+              value={formData.email}
+              onChange={(e) => handleChange(e)}
+            />
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="tel">Telefone</label>
+            <input
+              type="tel"
+              name="tel"
+              value={formData.tel}
+              onChange={(e) => handleChange(e)}
+            />
+          </div>
+
+          <button type="submit">Salvar</button>
+          <input type="hidden" name="_csrf" value={csrfToken} />
+        </form>
+      </S.Form>
+    </S.Container>
+  );
+};
+Edit.propTypes = {
+  csrfToken: P.string.isRequired,
 };
---
+++ src/components/Edit/styles.js
@@ -0,0 +1,75 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  width: 100%;
+  height: calc(100vh - 60px);
+
+  padding: 0.875rem;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Form = styled.div`
+  width: 370px;
+  height: 350px;
+
+  h2 {
+    font: var(--poppins-32);
+  }
+
+  form {
+    width: 100%;
+    height: 100%;
+
+    gap: 0.875rem;
+    display: flex;
+    align-items: start;
+    flex-direction: column;
+    justify-content: center;
+
+    .form-group {
+      width: 100%;
+
+      display: flex;
+      flex-direction: column;
+
+      label {
+      }
+
+      input {
+        height: 40px;
+
+        padding: 0.575rem;
+
+        border: 1px solid var(--color-4);
+        border-radius: 4px;
+
+        &:focus {
+          outline: none;
+
+          border: 1px solid var(--color-p5);
+          transition: border 0.3s ease-in-out, box-shadow 0.3s ease-in-out;
+        }
+
+        &::placeholder {
+          font-family: "Poppins", sans-serif;
+          font-size: 15px;
+        }
+      }
+    }
+
+    button {
+      width: 100px;
+      height: 35px;
+
+      font: var(--poppins-16);
+      color: var(--color-0);
+
+      border: none;
+      border-radius: 4px;
+      background: var(--color-p5);
+    }
+  }
+`;
